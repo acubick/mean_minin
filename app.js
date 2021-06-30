71d525c1d5d3c84d8a591e463c7dc24bd8ab1235
acubick <acubick@gmail.com>
2021-06-30 03:06:38 +0300

    Add nodemon

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//                          msg: 'Working'
+//                        })
+// })
+
+
+
+module.exports = app
