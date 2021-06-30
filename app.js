ec6acb027a444adce1fb24b00685da49410dda6b
acubick <acubick@gmail.com>
2021-06-30 06:38:04 +0300

    MongoDB add authorisation

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express         = require('express')
-const bodyParser = require('body-parser')
+const mongoose        = require('mongoose')
+const bodyParser      = require('body-parser')
 const authRoutes      = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes  = require('./routes/category')
 const orderRoutes     = require('./routes/order')
 const positionRoutes  = require('./routes/position')
 const app             = express()
+const keys = require('./config/keys')
 
+mongoose.connect(keys.mongoURI)
+        .then(()=> console.log("MongoDb connected." ))
+        .catch(err => console.log(err))
 
 app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
