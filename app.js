2cb2b5bb9e463e14a1d2695abd689d3c2a957469
acubick <acubick@gmail.com>
2021-06-30 03:37:57 +0300

    Add routes login and register

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
-
+const authRoutes = require('./routes/auth')
 const app = express()
+
+
+//  localhost:5000/api/auth/login
+app.use('/api/auth', authRoutes)
+
+
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({
