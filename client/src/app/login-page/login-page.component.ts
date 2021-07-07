bc155146011c3544f3c7c264bdff17ab8d532eb2
acubick <acubick@gmail.com>
2021-07-07 16:31:05 +0300

    Add auth service

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,28 +1,59 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core'
 import {FormControl, FormGroup, Validators} from "@angular/forms"
+import {Subscription} from "rxjs"
+import {ActivatedRoute, Router} from "@angular/router"
+
+import {AuthService} from "../shared/services/auth.service"
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   // @ts-ignore
   form: FormGroup
 
+  aSub: Subscription = new Subscription
 
-  constructor() {
+
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute ) {
   }
+
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      email: new FormControl(null, [Validators.required, Validators.email ]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(4)]),
+    })
+    this.route.queryParams.subscribe((params) => {
+      if(params['registered']){
+        // Теперь вы можете войти в систему используя свои данные
+      }else if(params['accessDenied']){
+        // Для начала авторизуйтесь в системе
+      }
     })
   }
 
-  onSubmit() {
+  ngOnDestroy(): void {
+    if (this.aSub) {
+      this.aSub.unsubscribe()
+    }
 
   }
+
+  onSubmit() {
+    this.form.disable()
+    this.aSub = this.auth.login(this.form.value).subscribe(
+      () => this.router.navigate(['/overview']),
+      error => {
+        console.warn(error)
+        this.form.enable()
+      }
+    )
+  }
+
 }
