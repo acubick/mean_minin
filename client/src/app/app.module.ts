f17474241e3a0a4cca50d1fe4adc9fc9c4e222b0
acubick <acubick@gmail.com>
2021-07-08 04:50:26 +0300

    Add token interceptor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 import {FormsModule, ReactiveFormsModule} from "@angular/forms"
-import {HttpClientModule} from "@angular/common/http"
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http"
 
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
-import {LoginPageComponent} from './login-page/login-page.component';
-import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
-import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
-import { RegisterPageComponent } from './register-page/register-page.component'
+import {LoginPageComponent} from './login-page/login-page.component'
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component'
+import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component'
+import {RegisterPageComponent} from './register-page/register-page.component'
 import {AuthService} from "./shared/services/auth.service"
+import {TokenInterceptor} from "./shared/classs/token.interceptor"
 
 @NgModule({
   declarations: [
@@ -26,7 +27,13 @@ import {AuthService} from "./shared/services/auth.service"
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: TokenInterceptor
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
