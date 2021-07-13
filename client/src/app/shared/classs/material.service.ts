21ebb58afc7882c37c91f7865671f561921874a4
acubick <acubick@gmail.com>
2021-07-13 19:21:43 +0300

    1 Creating layout

--- client/src/app/shared/classs/material.service.ts
+++ client/src/app/shared/classs/material.service.ts
@@ -25,6 +25,9 @@ export class MaterialService {
     // @ts-ignore
     return M.Modal.init(ref.nativeElement)
   }
+  static initTooltip(ref: ElementRef): MaterialInstance{
+    return M.Tooltip.init(ref.nativeElement)
+  }
 
 }
 
