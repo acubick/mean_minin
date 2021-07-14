5af8d4918531419e9cb511756a6d6d9e61af04d0
acubick <acubick@gmail.com>
2021-07-14 18:11:28 +0300

    1 Calculating analytics data

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,11 +1,10 @@
-const express = require('express')
-
+const express  = require('express')
+const passport = require('passport')
+const router   = express.Router()
 
 const controller = require('../conrollers/analytics')
 
-const router  = express.Router()
-
-router.get('/overview', controller.overview)
-router.get('/analytics', controller.analytics)
+router.get('/overview', passport.authenticate('jwt', {session: false}), controller.overview)
+router.get('/analytics', passport.authenticate('jwt', {session: false}), controller.analytics)
 
 module.exports = router
